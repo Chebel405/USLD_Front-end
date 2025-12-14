1013854614bd5c9c15f129e12c14dc89519b4462
Chebel405 <anne.chebel@gmail.com>
2025-12-14 16:01:34 +0100

    Ajout de l'affichage des patients Alzheimer et Sans Soin + Design tableau

--- src/app/components/patients/alzheimer/list-patient-alzheimer/list-patient-alzheimer.component.ts
+++ src/app/components/patients/alzheimer/list-patient-alzheimer/list-patient-alzheimer.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { PatientAlzheimer } from '../../../../models/patient-alzheimer.model';
+import { PatientAlzheimerService } from '../../../../services/patient-alzheimer.service';
 
 @Component({
   selector: 'app-list-patient-alzheimer',
-  imports: [],
   templateUrl: './list-patient-alzheimer.component.html',
-  styleUrl: './list-patient-alzheimer.component.css'
+  styleUrls: ['./list-patient-alzheimer.component.css'],
+  standalone: true,
+  imports: [CommonModule]
 })
-export class ListPatientAlzheimerComponent {
+export class ListPatientAlzheimerComponent implements OnInit {
+  patients: PatientAlzheimer[] = [];
 
+  constructor(private alzheimerService: PatientAlzheimerService) { }
+
+  ngOnInit(): void {
+    this.alzheimerService.findAll().subscribe({
+      next: (data) => this.patients = data,
+      error: (err) => console.error('Erreur chargement Alzheimer', err)
+    });
+  }
 }
