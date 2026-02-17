091e0cbe79c860005eafd452f3f2da2d66df7e50
Chebel405 <anne.chebel@gmail.com>
2026-02-17 17:46:13 +0100

    Le logo est un bouton qui ramene vers '/home'

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../../services/auth.service';
 
 @Component({
     selector: 'app-navbar',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, RouterLink],
     templateUrl: './navbar.component.html',
     styleUrls: ['./navbar.component.css']
 })
